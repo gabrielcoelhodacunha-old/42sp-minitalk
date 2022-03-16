a26950cae6d8e2f44eb53b2b74805e70012344b9
Gabriel C. Cunha <gcoelho-@student.42sp.org.br>
2022-03-16 18:29:59 -0300

    Restarted project

--- client/src/main.c
+++ client/src/main.c
@@ -2,17 +2,17 @@
 
 void	check_args(int argc, char **argv);
 void	handle_sigusr(int signal_number);
-void	configure_signal_action(void);
 void	send_message(pid_t server_pid, char *message);
 
 int	main(int argc, char **argv)
 {
 	pid_t	server_pid;
+	char	*message;
 
 	check_args(argc, argv);
 	server_pid = ft_atoi(argv[1]);
-	configure_signal_action();
-	send_message(server_pid, argv[2]);
+	message = ft_strdup(argv[2]);
+	send_message(server_pid, message);
 	exit(EXIT_SUCCESS);
 }
 
@@ -31,41 +31,18 @@ void	check_args(int argc, char **argv)
 	exit(EXIT_FAILURE);
 }
 
-void	configure_signal_action(void)
-{
-	struct sigaction	signal_action;
-
-	signal_action.sa_flags = 0;
-	signal_action.sa_handler = handle_sigusr;
-	sigaction(SIGUSR1, &signal_action, NULL);
-	sigaction(SIGUSR2, &signal_action, NULL);
-}
-
 void	handle_sigusr(int signal_number)
 {
-	ft_printf("%i", signal_number == SIGUSR1);
 }
 
 void	send_message(pid_t server_pid, char *message)
 {
-	char	idx;
-	int	signal_number;
+	struct sigaction	signal_action;
 
-	while (*message)
-	{
-		ft_printf("%c : ", *message);
-		idx = 8;
-		while (--idx >= 0)
-		{
-			if (*message & (1 << idx))
-				signal_number = SIGUSR2;
-			else
-				signal_number = SIGUSR1;
-			ft_printf("%i", signal_number == SIGUSR2);
-			kill(server_pid, signal_number);
-		}
-		ft_printf("\n");
-		message++;
-	}
-	//pause();
+	if (!message)
+		exit(EXIT_FAILURE);
+	signal_action.sa_flags = 0;
+	signal_action.sa_handler = handle_sigusr;
+	sigaction(SIGUSR1, &signal_action, NULL);
+	sigaction(SIGUSR2, &signal_action, NULL);
 }
--- server/src/main.c
+++ server/src/main.c
@@ -1,15 +1,14 @@
 #include "server.h"
 
 void	show_pid(void);
+void	receive_signal(int signal_number, pid_t client_pid);
 void	handle_sigusr(int signal_number, siginfo_t *info, void *context);
-void	configure_signal_action(void);
+void	receive_message(void);
 
 int	main(void)
 {
 	show_pid();
-	configure_signal_action();
-	while (1)
-		;
+	receive_message();
 	exit(EXIT_SUCCESS);
 }
 
@@ -18,7 +17,18 @@ void	show_pid(void)
 	ft_printf("PID : %i\n", getpid());
 }
 
-void	configure_signal_action(void)
+void	receive_signal(int signal_number, pid_t client_pid)
+{
+	(void) client_pid;
+}
+
+void	handle_sigusr(int signal_number, siginfo_t *info, void *context)
+{
+	(void) context;
+	receive_signal(signal_number, info->si_pid);
+}
+
+void	receive_message(void)
 {
 	struct sigaction	signal_action;
 
@@ -26,27 +36,6 @@ void	configure_signal_action(void)
 	signal_action.sa_sigaction = handle_sigusr;
 	sigaction(SIGUSR1, &signal_action, NULL);
 	sigaction(SIGUSR2, &signal_action, NULL);
-}
-
-void	set_bit(char *message, size_t idx)
-{
-	size_t	midx;
-	size_t	shift;
-
-	midx = sizeof(idx) * 8;
-	shift = idx % midx;
-	midx = idx / midx;
-	message[midx] |= 128 >> shift;
-}
-
-void	receive_message(int signal_number, siginfo_t *info)
-{
-	(void) info;
-	ft_printf("%i", signal_number == SIGUSR2);
-}
-
-void	handle_sigusr(int signal_number, siginfo_t *info, void *context)
-{
-	(void) context;
-	receive_message(signal_number, info);
+	while (1)
+		;
 }
